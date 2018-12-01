83a3b4d6020dd428e16a8c1f3736200b6801bba8
Promita3803NY <promi_taj@yahoo.com>
2018-12-01 15:29:08 -0500

    first git remote add origin https://github.com/Promita3803NY/HW1.git git push -u origin master git commit -m first

---
+++ src/hw1/intro.java
@@ -0,0 +1,15 @@
+package hw1;
+
+public class intro {
+	public static void main( String[] args) {
+		String first_name = "Promita";
+		String last_name = "Munni";
+		String student_id = "3803NY";
+		String address = "89-67,210 street,queens village ny-11427";
+		System.out.println(" My name is " +first_name+ " " +last_name);
+		System.out.println(" My student ID is " +student_id);
+		System.out.println(" I live in " +address);
+		
+		System.out.println(" My introduction: " +first_name+ " " +last_name+ " " +student_id+ " " +address);
+}
+}
